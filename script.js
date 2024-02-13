017924c221e6188d765893a57ff68e6e95976705
MiG21-5121 <quayoho@gmail.com>
2024-02-14 02:40:00 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,34 @@
+function createHeart() {
+    const heart = document.createElement('div');
+    heart.classList.add('heart');
+    
+    heart.style.left = Math.random() * 100 + "vw";
+    heart.style.animationDuration = Math.random() * 2 + 3 + "s";
+    
+    heart.innerText = '❤️';
+    
+    document.body.appendChild(heart);
+    
+    setTimeout(() => {
+                heart.remove();
+                }, 5000);
+  }
+  
+  setInterval(createHeart, 150);
+
+  //login
+
+  function login() {
+    const password = document.getElementById('passwordInput').value;
+
+    if (password === 'cihmotcloulsahtwem') {
+        // hiện ảnh
+        document.querySelector('.container2').style.display = 'flex';       
+
+        // Xóa trang login
+        document.querySelector('.container').style.display = 'none';
+    } else {
+        alert('Sai ròi ! Nhập lại đi nè :>');
+    }
+}
+
